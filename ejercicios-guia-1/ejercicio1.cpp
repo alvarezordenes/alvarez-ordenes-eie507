343e9860f28267f3697e7b152d9ba1c5e7a46dc5
alvarezordenes <143766010+alvarezordenes@users.noreply.github.com>
2023-09-22 21:49:08 -0300

    Create ejercicio1.cpp

---
+++ ejercicios-guia-1/ejercicio1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declaración de variables
+    string CAT;
+    int SAL, ANT, CLA;
+
+    // Solicita la categoría del empleado, la antigüedad, la clave y el salario
+    cout << "Ingrese la categoría del empleado (1, 2, 3 o 4): ";
+    cin >> CAT;
+    cout << "Ingrese la antigüedad del empleado en años: ";
+    cin >> ANT;
+    cout << "Ingrese la clave del empleado: ";
+    cin >> CLA;
+    cout << "Ingrese el salario del empleado: ";
+    cin >> SAL;
+
+    // Comprueba las condiciones
+    if ((CAT == 3 || CAT == 4) && ANT > 5) {
+        cout << "El empleado cumple con las condiciones para el puesto." << endl;
+    } else if (CAT == 2 && ANT > 7) {
+        cout << "El empleado cumple con las condiciones para el puesto." << endl;
+    } else {
+        cout << "El empleado no cumple con las condiciones para el puesto." << endl;
+    }
+
+    return 0;
+}
